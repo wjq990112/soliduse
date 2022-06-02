d8806bbecaec957909bdb5abef607caee675ed10
wjq990112 <1163585385@qq.com>
2022-06-03 00:53:01 +0800

    test(core): fix and add cases for SSR

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,5 @@
+export { default as useBreakpoints } from './useBreakpoints';
 export { default as useClickOutside } from './useClickOutside';
+export { default as useDocumentVisibility } from './useDocumentVisibility';
 export { default as useEventListener } from './useEventListener';
+export { default as useMediaQuery } from './useMediaQuery';
---
+++ packages/core/src/useEventListener/__tests__/index.ssr.test.tsx
@@ -0,0 +1,35 @@
+/// <reference types="vitest/globals" />
+
+import { cleanup, fireEvent, mount } from '@soliduse/shared';
+import useEventListener from '..';
+
+const isString = (value: unknown): value is string => typeof value === 'string';
+vi.mock('@soliduse/shared/dist/utils/is', () => {
+  return {
+    get isClient() {
+      return false;
+    },
+    get isString() {
+      return isString;
+    },
+  };
+});
+
+describe('@soliduse/core/useEventListener', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  test('should register event listener to Window by default correctly when server-side rendering', () => {
+    const listener = vi.fn();
+
+    function App() {
+      useEventListener('click', listener);
+      return null;
+    }
+
+    mount(App);
+    fireEvent.click(window);
+    expect(listener).toHaveBeenCalledTimes(0);
+  });
+});
